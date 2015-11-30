146cb8b6a5bfb7e41fad80aea535780cb6f69ad2
Abdullah Hashim <aprenticebutchr@gmail.com>
2015-11-30 10:10:24 +0000

    Renames certified_outlets to certified_outlet_list.

--- spec/hmc_outlets_spec.rb
+++ spec/hmc_outlets_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe HmcOutlets do
   
-  let(:certified_outlets) { ["outlet 1", "outlet 2", "outlet 4"] }
+  let(:certified_outlet_list) { ["outlet 1", "outlet 2", "outlet 4"] }
   let(:removed_outlets) { ["outlet 3", "outlet 5", "outlet 6"] }
   let(:non_existent_outlet) { "outlet 12" }
   
@@ -12,7 +12,7 @@ describe HmcOutlets do
   
   describe ".certified_outlet_list" do
     it "returns an array of all certified outlets" do
-      expect(HmcOutlets.certified_outlet_list).to eq(certified_outlets)
+      expect(HmcOutlets.certified_outlet_list).to eq(certified_outlet_list)
     end
   end
   
@@ -24,7 +24,7 @@ describe HmcOutlets do
   
   describe ".certified?(outlet_name)" do
     it "returns true for currently certified outlets" do
-      expect(HmcOutlets.certified?(certified_outlets[0])).to be_truthy
+      expect(HmcOutlets.certified?(certified_outlet_list[0])).to be_truthy
     end
     it "returns false for outlets that have been removed" do
       expect(HmcOutlets.certified?(removed_outlets[0])).to be_falsey
@@ -39,7 +39,7 @@ describe HmcOutlets do
       expect(HmcOutlets.revoked?(removed_outlets[0])).to be_truthy
     end 
     it "returns false for currently certified outlets" do
-      expect(HmcOutlets.revoked?(certified_outlets[0])).to be_falsey
+      expect(HmcOutlets.revoked?(certified_outlet_list[0])).to be_falsey
     end
     it "returns false for non-existant outlets" do
       expect(HmcOutlets.revoked?(non_existent_outlet)).to be_falsey
