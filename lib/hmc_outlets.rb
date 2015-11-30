8eef663960ddcb8cf20f281ded6463a194e50188
Abdullah Hashim <aprenticebutchr@gmail.com>
2015-11-30 12:20:42 +0000

    Adds methods to return more data about outlets.

--- lib/hmc_outlets.rb
+++ lib/hmc_outlets.rb
@@ -13,12 +13,92 @@ module HmcOutlets
     revoked_outlet_list.include?(outlet_name)
   end
   
+  def self.outlet_list
+    @outlet_list ||= outlets_page.css('table.outlettable tr td strong').map { |name| name.text.gsub(/[[:space:]]$/, '') }
+  end
+  
   def self.certified_outlet_list
-    @certified_outlets ||= outlets_page.css('table.outlettable tr td:not(.removed) strong').map { |name| name.text.gsub(/[[:space:]]$/, '') }
+    @certified_outlet_list ||= outlets_page.css('table.outlettable tr td:not(.removed) strong').map { |name| name.text.gsub(/[[:space:]]$/, '') }
   end
   
   def self.revoked_outlet_list
     @removed_outlets ||= outlets_page.css('table.outlettable tr td.removed strong').map { |name| name.text.gsub(/[[:space:]]$/, '') }
+  end
+  
+  def self.outlets
+    unless @outlets
+      @outlets = Array.new
+      outlets = outlets_page.css('table.outlettable tr')
+      outlets.each do |o|
+        if o.css('strong').size > 0
+          name = o.css('strong').map { |n| n.text.gsub(/[[:space:]]$/, '') }
+          phone = o.css('.tel').map { |n| n.text.gsub(/[[:space:]]$/, '') }
+          address = o.css('td[4]').map do |n| 
+            text = n.text
+            text.gsub!(/\t/, '')
+            text.gsub!(/,/, '')
+            text.split("\n")
+          end
+          street = address[0][0] if address[0]
+          city = address[0][1] if address[0]
+          county = address[0][2] if address[0]
+          postcode = address[0][3] if address[0]
+          status = o.css('td.removed').size == 0 ? "certified" : "revoked"
+          @outlets << { name: name[0], phone: phone[0], street: street, city: city, county: county, postcode: postcode, status: status }
+        end
+      end
+    end
+    @outlets
+  end
+  
+  def self.certified_outlets
+    unless @certified_outlets
+      @certified_outlets = Array.new
+      outlets = outlets_page.css('table.outlettable tr')
+      outlets.each do |o|
+        if o.css('td:not(.removed)').size > 0
+          name = o.css('strong').map { |n| n.text.gsub(/[[:space:]]$/, '') }
+          phone = o.css('.tel').map { |n| n.text.gsub(/[[:space:]]$/, '') }
+          address = o.css('td[4]').map do |n| 
+            text = n.text
+            text.gsub!(/\t/, '')
+            text.gsub!(/,/, '')
+            text.split("\n")
+          end
+          street = address[0][0] if address[0]
+          city = address[0][1] if address[0]
+          county = address[0][2] if address[0]
+          postcode = address[0][3] if address[0]
+          @certified_outlets << { name: name[0], phone: phone[0], street: street, city: city, county: county, postcode: postcode }
+        end
+      end
+    end
+    @certified_outlets
+  end
+  
+  def self.revoked_outlets
+    unless @revoked_outlets
+      @revoked_outlets = Array.new
+      outlets = outlets_page.css('table.outlettable tr')
+      outlets.each do |o|
+        if o.css('td:not(.removed)').size == 0
+          name = o.css('strong').map { |n| n.text.gsub(/[[:space:]]$/, '') }
+          phone = o.css('.tel').map { |n| n.text.gsub(/[[:space:]]$/, '') }
+          address = o.css('td[4]').map do |n| 
+            text = n.text
+            text.gsub!(/\t/, '')
+            text.gsub!(/,/, '')
+            text.split("\n")
+          end
+          street = address[0][0] if address[0]
+          city = address[0][1] if address[0]
+          county = address[0][2] if address[0]
+          postcode = address[0][3] if address[0]
+          @revoked_outlets << { name: name[0], phone: phone[0], street: street, city: city, county: county, postcode: postcode } if name[0]
+        end
+      end
+    end
+    @revoked_outlets
   end
   
   private
--- spec/hmc_outlets_spec.rb
+++ spec/hmc_outlets_spec.rb
@@ -5,10 +5,57 @@ describe HmcOutlets do
   let(:certified_outlet_list) { ["outlet 1", "outlet 2", "outlet 4"] }
   let(:revoked_outlet_list) { ["outlet 3", "outlet 5", "outlet 6"] }
   let(:non_existent_outlet) { "outlet 12" }
-  
+  let(:outlets) do 
+    [ 
+      { name: "outlet 1", phone: "phonenumber1", street: "address1", city: "city1", county: "county1", postcode: "postcode1", status: "certified" },
+      { name: "outlet 2", phone: "phonenumber2", street: "address2", city: "city2", county: "county2", postcode: "postcode2", status: "certified" },
+      { name: "outlet 3", phone: "phonenumber3", street: "address3", city: "city3", county: "county3", postcode: "postcode3", status: "revoked" },
+      { name: "outlet 4", phone: "phonenumber4", street: "address4", city: "city4", county: "county4", postcode: "postcode4", status: "certified" },
+      { name: "outlet 5", phone: "phonenumber5", street: "address5", city: "city5", county: "county5", postcode: "postcode5", status: "revoked" },
+      { name: "outlet 6", phone: "phonenumber6", street: "address6", city: "city6", county: "county6", postcode: "postcode6", status: "revoked" }
+    ]
+  end
+  let(:certified_outlets) do 
+    [ 
+      { name: "outlet 1", phone: "phonenumber1", street: "address1", city: "city1", county: "county1", postcode: "postcode1" },
+      { name: "outlet 2", phone: "phonenumber2", street: "address2", city: "city2", county: "county2", postcode: "postcode2" },
+      { name: "outlet 4", phone: "phonenumber4", street: "address4", city: "city4", county: "county4", postcode: "postcode4" }
+    ]
+  end
+  let(:revoked_outlets) do 
+    [ 
+      { name: "outlet 3", phone: "phonenumber3", street: "address3", city: "city3", county: "county3", postcode: "postcode3" },
+      { name: "outlet 5", phone: "phonenumber5", street: "address5", city: "city5", county: "county5", postcode: "postcode5" },
+      { name: "outlet 6", phone: "phonenumber6", street: "address6", city: "city6", county: "county6", postcode: "postcode6" }
+    ]
+  end
   
   specify {expect(HmcOutlets::VERSION).not_to be nil }
   specify { expect(HmcOutlets::CERTIFIED_OUTLETS_URI).to eq "http://www.halalhmc.org/certified-outlets-results.htm#result" }
+  
+  describe ".outlets" do
+    it "returns a hash with the name, telephone and address of all outlets (certified/revoked)" do
+      expect(HmcOutlets.outlets).to eq(outlets)
+    end
+  end
+  
+  describe ".certified_outlets" do
+    it "returns a hash with the name, telephone and address of certified outlets" do
+      expect(HmcOutlets.certified_outlets).to eq(certified_outlets)
+    end
+  end
+  
+  describe ".revoked_outlets" do
+    it "returns a hash with the name, telephone and address of revoked outlets" do
+      expect(HmcOutlets.revoked_outlets).to eq(revoked_outlets)
+    end
+  end
+  
+  describe ".outlet_list" do
+    it "returns an array of all outlets (certifed/revoked)" do
+      expect(HmcOutlets.outlet_list).to eq((certified_outlet_list + revoked_outlet_list).sort)
+    end
+  end
   
   describe ".certified_outlet_list" do
     it "returns an array of all certified outlets" do
